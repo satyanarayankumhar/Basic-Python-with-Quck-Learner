c7a1a9beb1c490d47be538131c5d0e6720a33319
Silent_Learner <53419562+satyanarayankumhar@users.noreply.github.com>
2023-09-17 10:27:15 +0530

    Add files via upload

---
+++ Even odd.py
@@ -0,0 +1,12 @@
+# Tuple datastructure in python
+
+tup1=(1,True,2.14,5-7j)
+print(tup1)
+
+print(type(tup1))
+
+tup1=(1,2,3,4)
+tup2=(3,4,5,6)
+print("merge the tuple",tup1+tup2)
+
+print("tup2 Repeating =",tup2*3)
