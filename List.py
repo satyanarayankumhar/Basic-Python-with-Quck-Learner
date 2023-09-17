f1cb5c81078e477c733e26a8b63f92cc165496fc
Silent_Learner <53419562+satyanarayankumhar@users.noreply.github.com>
2023-09-17 10:31:09 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,27 @@
+#create a list and some operation perform
+
+# l=[1,5,8,[1,2,5],"Sudhansh",345.50,(5+5j),False]
+# l1=["satya",390,50.50]
+# s='pwskills'
+# l2=[2,5,9]
+# n=50
+# # print(l)
+# # print(l[::-1])
+# # print(str(l[7])[0:3])
+# # print(l+l1)
+#
+# print(l)
+# print(l1)
+# l.append(l1)
+#
+# l.append(s)
+# print(l)
+# print(l[8][1])
+
+#Tuples are also Data type of python
+
+tuple=(1,3,7,["hello",78,True],5+7j,"tuples",78.09,{"hi", 50})
+print(tuple)
+print(tuple.index("tuples"))
+
+
